b9fffbdad96451794b8f91704945aa08d7db36f2
Pavel Dobranov <pdobranov@codix.eu>
2020-03-25 20:53:37 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const gulp = require('gulp');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const purgeCss = require('gulp-purgecss');
+const cleanCss = require('gulp-clean-css');
+
+const browserSync = require('browser-sync');
+const del = require('del');
+
+const paths = {
+  pages: {
+    src: './src/pages/**/*.ejs',
+    dest: './dist'
+  },
+  styles: {
+    src: './src/styles/styles.scss',
+    dest: './dist/styles'
+  },
+  images: {
+    src: './src/images/**/*',
+    dest: './dist/images'
+  }
+};
+
+const server = browserSync.create();
+
+const reload = done => {
+  server.reload();
+  done();
+};
+
+const serve = done => {
+  server.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  done();
+};
+
+const clean = () => del(['dist']);
+
+const pages = () => {
+  return gulp.src(paths.pages.src)
+    .pipe(ejs())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest(paths.pages.dest));
+};
+
+const stylesDev = () => {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+const stylesBuild = () => {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe(purgeCss({
+      content: ['./src/pages/**/*.ejs']
+    }))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+const images = () => {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+const watch = () => {
+  gulp.watch([paths.pages.src], gulp.series(pages, reload));
+  gulp.watch([paths.styles.src], gulp.series(stylesDev, reload));
+  gulp.watch([paths.images.src], gulp.series(images, reload));
+};
+
+gulp.task('dev', gulp.series(clean, pages, stylesDev, images, serve, watch));
+
+gulp.task('build', gulp.series(clean, pages, stylesBuild, images));
